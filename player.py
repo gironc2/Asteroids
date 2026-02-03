9759ea2b2906efa1170734d6df6dea9686fa2f1e
gironc2 <gironc2@gmail.com>
2026-02-02 18:32:35 -0800

    G: add shooting cd

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
 
     # in the Player class
     def triangle(self):
@@ -25,6 +26,8 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        self.shot_cooldown_timer -= dt
+
         if keys[pygame.K_a]:
             # ?
             self.rotate(dt)
@@ -40,7 +43,11 @@ class Player(CircleShape):
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_cooldown_timer > 0:
+                pass
+            else:
+                self.shoot()
+                self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
